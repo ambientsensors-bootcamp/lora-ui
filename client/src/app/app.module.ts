182f863fea27bfc35cb73ee54e83f2f9c589020f
Madia <madiamm1@gmail.com>
2017-07-16 14:36:33 -0600

    Fixed the issues

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ComponentComponent } from './navigation/component/component.component';
 import { NavigationComponent } from './component/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentComponent,
     NavigationComponent
   ],
   imports: [
