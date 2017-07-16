9257bee3c4dd9f6f2f154c89fb7194a58be80d2d
Madia <madiamm1@gmail.com>
2017-07-16 14:23:44 -0600

    Navigation created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './navigation/component/component.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
