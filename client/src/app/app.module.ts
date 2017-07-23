f06d1e48cd98779cbbbf6de6dbf15486d96e96c0
Raghad Al Fatal <raghadalfattal@gmail.com>
2017-07-23 12:23:39 -0600

    Adding routing with angular2 material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MaterialModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './component/navigation/navigation.component';
 import { DevicesComponent } from './component/devices/devices.component';
 import { ConnectionsComponent } from './component/connections/connections.component';
 import { LocationMapComponent } from './component/location-map/location-map.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,8 +18,8 @@ import { LocationMapComponent } from './component/location-map/location-map.comp
     LocationMapComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,MaterialModule , BrowserAnimationsModule, AppRoutingModule
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
