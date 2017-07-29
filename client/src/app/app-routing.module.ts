59e955624389e7d74c32c72e4ae6df78ea74b7ef
Sharihan ali <sharihanali@u.boisestate.edu>
2017-07-29 15:00:10 -0600

    collections linked in routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { CollectionsComponent} from './components/collections/collections.component'
 
 
 const routes: Routes =
     [
      
         { path: 'about', component: AboutComponent},
+        { path: 'collections', component: CollectionsComponent},
         { path: '', pathMatch: 'full', redirectTo: 'about' },
         { path: '**', pathMatch: 'full', redirectTo: 'about' }
     ];
