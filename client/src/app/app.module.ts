6fbcbc00d53986d014e6b5dd3900249c0d2a585a
Sharihan ali <sharihanali@u.boisestate.edu>
2017-07-30 14:11:12 -0600

    Fix menu routers and add provider for ws

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { WebServiceClient } from './services/webServerClient';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { MaterialModule } from '@angular/material';
@@ -18,7 +18,7 @@ import { CollectionsComponent } from './components/collections/collections.compo
     MaterialModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [WebServiceClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
