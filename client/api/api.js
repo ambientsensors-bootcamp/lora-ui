fa77a9740f333f833895c7c3ce6a17320282a0a0
merna1 <merna1@github.com>
2017-08-06 15:16:59 -0600

    Added /find endpoint to express

--- client/api/api.js
+++ client/api/api.js
@@ -4,7 +4,9 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
 
+const MONGODB_URI = 'mongodb://<dbuser>:<dbpassword>@ds143241.mlab.com:43241/lorauidb';
 /**
  * TODO: We're going to define paths below
  */
@@ -21,6 +23,24 @@ router.get('/', function (req, res, next) {
  */
 router.get('/about', function (req, res, next) {
   res.send({ "version": "0.1.0" });
+});
+
+
+/**
+ * http://localhost:3000/api/find
+ */
+router.get('/find', function (req, res, next) {
+  MongoClient.connect(MONGODB_URI, function (err, db) {
+    if (err) throw err
+
+    db.collection('books').find().toArray(function (err, result) {
+      if (err) throw err
+
+      console.log(result);
+      res.send(result);
+    })
+  })
+
 });
 
 
